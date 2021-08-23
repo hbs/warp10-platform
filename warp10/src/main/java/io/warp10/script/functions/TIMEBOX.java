eae3ffa6edaa36a559539413cf69e7bc147cc02d
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-08-23 12:58:25 +0200

    Added support for signals

--- warp10/src/main/java/io/warp10/script/functions/TIMEBOX.java
+++ warp10/src/main/java/io/warp10/script/functions/TIMEBOX.java
@@ -31,6 +31,7 @@ import io.warp10.script.NamedWarpScriptFunction;
 import io.warp10.script.WarpScriptException;
 import io.warp10.script.WarpScriptStack;
 import io.warp10.script.WarpScriptStack.Macro;
+import io.warp10.script.WarpScriptStack.Signal;
 import io.warp10.script.WarpScriptStackFunction;
 import io.warp10.warp.sdk.Capabilities;
 
@@ -55,8 +56,16 @@ public class TIMEBOX extends NamedWarpScriptFunction implements WarpScriptStackF
    */
   private static final String SPECIAL_ALLOWANCE_CAPNAME = WarpConfig.getProperty(Configuration.MAXTIME_EXTENSION_ALLOWANCE_CAPNAME);
 
+  private final Signal signal;
+
   public TIMEBOX(String name) {
     super(name);
+    this.signal = null;
+  }
+
+  public TIMEBOX(String name, Signal signal) {
+    super(name);
+    this.signal = signal;
   }
 
   @Override
@@ -101,6 +110,9 @@ public class TIMEBOX extends NamedWarpScriptFunction implements WarpScriptStackF
     try {
       future.get(maxtime, TimeUnit.MILLISECONDS);
     } catch (TimeoutException te) {
+      if (null != signal) {
+        stack.signal(signal);
+      }
       throw new WarpScriptException(getName() + " reached the execution time limit (" + maxtime + " ms).");
     } catch (ExecutionException ee) {
       throw new WarpScriptException(getName() + " encountered an exception while executing macro", ee.getCause());
